ecbef890dd7327853894e3e0eb836402c20b3fde
pdeven <103482743+pdeven@users.noreply.github.com>
2023-08-22 11:57:47 +0530

    Update plot.R
    Converting pdf to image

--- src/plot.R
+++ src/plot.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 library(circlize)
 library(optparse)
-
+library(pdftools)
 option_list = list(
   make_option(c("-l", "--left_bed"),help="regions showing breakpoint of left gene"),
   make_option(c("-r", "--right_bed"),help="regions showing breakpoint of right gene"),
@@ -15,24 +15,23 @@ pdf_output <- opt$pdf
 df1=read.csv(left, sep = "\t")
 df2=read.csv(right, sep = "\t")
 df=rbind(df1, df2)
-
 pdfWidth=list("pdfWidth", "numeric", 200)
 pdfHeight=list("pdfHeight", "numeric", 100)
 
 pdf(pdf_output , width = pdfWidth, height = pdfHeight, title = "Fusions")
-
 circos.initializeWithIdeogram()
-#circos.initializeWithIdeogram(plotType = c("labels", "axis"))
-
 circos.genomicLabels(df, 
                      labels.column = 4, 
                      side = "outside",
                      col = as.numeric(factor(df[[1]])), 
                      cex = 0.1,
-                     line_col = as.numeric(factor(df[[1]])))
+                    line_col = as.numeric(factor(df[[1]])))
 circos.genomicLink(df1, df2, col = rand_color(nrow(df1), transparency = 0), 
                    border = NA, lwd=1)
 mtext("    GRCh37\n", outer=TRUE,  cex=0.8, line=-2,side=3,adj=0,font = 4)
 mtext("     Fusion Genes", outer=TRUE,  cex=0.7, line=-2.5,side=3,adj=0,font = 3)
 dev.off()
+
+pdf_convert(pdf_output, format="jpeg", dpi=900, filenames = gsub(".pdf", "_image.jpeg", pdf_output))
+
 
